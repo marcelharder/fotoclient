d4ba5128bd8595b4f358bce4d3a8888f99851b57
marcel <marcelharder@protonmail.com>
2024-07-05 15:10:35 +0200

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from "./nav/nav.component";
+import { AccountService } from './_services/account.service';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, NavComponent, HomeComponent]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
+  http = inject(HttpClient);
+  private accountService = inject(AccountService);
   title = 'client';
+  result: any;
+
+
+  ngOnInit(): void {
+   this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
+  }
+
+  getDescription(){
+    this.http.get<string>('http://localhost:5123/api/Config/getDescription/4', {responseType: 'text' as 'json'}).subscribe({
+      next: response => this.result = response,
+      error: error=> console.log(error),
+      complete: ()=> console.log('Request has completed')
+    })
+  }
 }
